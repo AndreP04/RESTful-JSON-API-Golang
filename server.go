834415a4b8b9993fd0a0b867c818c2883c170c4d
AndreP04 <andrepret4@icloud.com>
2024-08-12 19:12:29 +0200

    Create model and send back JSON

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	
+	"time"
+
 	//Import router
 	"github.com/gorilla/mux"
 )
@@ -18,12 +20,31 @@ func main() {
 	log.Fatal(http.ListenAndServe(":80", router))
 }
 
+//Create a model
+type Todo struct {
+	Name string
+	Completed bool
+	Due time.Time
+}
+
+//Create toDo data type (slice)
+type Todos []Todo
+
+//Add more endpoints
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome")
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "To Do Index")
+
+	//Send back JSON
+	todos := Todos {
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
