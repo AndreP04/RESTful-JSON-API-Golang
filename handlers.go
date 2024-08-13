e9cb910618f3ff22da5eae9ca741d6abd593d69d
AndreP04 <andrepret4@icloud.com>
2024-08-13 20:03:44 +0200

    Bug fixes

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -31,14 +32,14 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 //"create" endpoint
 func TodoCreate(w http.ResponseWriter, r *http.Request) {
     var todo Todo
-    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    body, err := io.ReadAll(io.LimitReader(r.Body, 1048576))
     if err != nil {
         panic(err)
     }
     if err := r.Body.Close(); err != nil {
         panic(err)
     }
-    if err := json.Unmarshal(body, &amp;todo); err != nil {
+    if err := json.Unmarshal(body, &todo); err != nil {
         w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(422) // unprocessable entity
         if err := json.NewEncoder(w).Encode(err); err != nil {
